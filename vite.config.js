56a4beb48e8ef5e4d0d1bec0b3909c3a614011f9
cynthia <glean@cynthiateeters.com>
2021-10-04 16:59:29 -0400

    Change from json to csv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 // vite.config.js
+import content from '@originjs/vite-plugin-content';
+
 module.exports = {
   root: 'src',
   build: {
     outDir: '../dist',
   },
+  plugins: [
+    content(),
+    /* options */
+  ],
 };
